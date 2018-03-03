83b600866a29e9f67c2cae1fe698e5e481733e42
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-03-03 17:18:35 +0200

    About us WIP

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import "./less/index.less"
 import React from "react"
 import Slider from '../components/slider'
+import AboutUs from '../components/about-us'
 
 export default () => <div>
-  <div>
-    <Slider />
-  </div>
+  <Slider />
+  <AboutUs />
 
-  <div className='dummy'>1231</div>
+  <div className='dummy'></div>
 </div>
