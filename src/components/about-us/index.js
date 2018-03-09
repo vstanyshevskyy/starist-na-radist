d1cd509d5e9883b37edbebac4fa08ba8c92b86b6
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-03-09 15:42:29 +0200

    About us fixes

--- src/components/about-us/index.js
+++ src/components/about-us/index.js
@@ -9,15 +9,17 @@ class AboutUs extends React.Component {
 
   render() {
     return <div className='about-us'>
-      <div className="about-us__image" style={{backgroundImage: `url(${imgPath})`}}>
+      <div className="about-us__image"
+        role="img" aria-label="Фото волонтерів та жителів пансіонату"
+        style={{backgroundImage: `url(${imgPath})`}}>
       </div>
-      <div className='about-us__content-wrapper'>
+      <div className='about-us__wrapper'>
         <div className='about-us__content'>
           <h3>Про нас</h3>
           <h5>Наша місія</h5>
           <p>Благодійна неприбуткова організація "Старість НА радість" це група волонтерів, що робить все можливе риба тут і далі лорем іпсум, потім перепишем</p>
           <p>І ще трохи тексту тут і далі щоб май файно виглядало, а потім буде кнопка!</p>
-          <a href='#' className="btn btn--orange">Детальніше про організацію</a>
+          <a href='#' className="btn btn--orange btn--large about-us__read-more">Детальніше про організацію</a>
         </div>
       </div>
     </div>
