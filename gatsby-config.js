a156920909bf9a0e31d538556c5b8cf42bdd55a9
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-05-04 16:31:52 +0300

    Setup transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,14 @@
 module.exports = {
-  plugins: [`gatsby-plugin-netlify-cms`, `gatsby-plugin-less`],
+  plugins: [
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-less`,
+    'gatsby-transformer-remark',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'settings',
+        path: `${__dirname}/content/settings/`
+      }
+    },
+  ],
 };
