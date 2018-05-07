f8f49b94b62c0b3e19f4803bb509a422c77e2678
Vitaliy Stanyshevskyy <vstanyshevskyy@stelladot.com>
2018-05-07 14:59:28 +0300

    Social links - show link if data passed in

--- src/components/social-links/index.js
+++ src/components/social-links/index.js
@@ -8,15 +8,27 @@ class SocialLinks extends React.Component {
   render() {
     const linkModifier = this.props.linksType || 'default';
     return <div className="social-links">
-      <a href="https://www.facebook.com/pg/staristNAradist" className={`social-links__link social-links__link--facebook social-links__link--${linkModifier}`}>
-        <i className="icon icon-facebook-with-circle"></i>
-      </a>
-      <a href="#" className={`social-links__link social-links__link--instagram social-links__link--${linkModifier}`}>
-        <i className="icon icon-instagram-with-circle"></i>
-      </a>
-      <a href="https://www.youtube.com/channel/UCydvBI5YmJPtRv5yzsQmIdA" className={`social-links__link social-links__link--youtube social-links__link--${linkModifier}`}>
-        <i className="icon icon-youtube-with-circle"></i>
-      </a>
+      {
+        this.props.facebookLink 
+          ? <a href={this.props.facebookLink} className={`social-links__link social-links__link--facebook social-links__link--${linkModifier}`}>
+            <i className="icon icon-facebook-with-circle"></i>
+          </a>
+        : null
+      }
+      {
+        this.props.instagramLink
+          ? <a href={this.props.instagramLink} className={`social-links__link social-links__link--instagram social-links__link--${linkModifier}`}>
+            <i className="icon icon-instagram-with-circle"></i>
+          </a>
+        : null
+      }
+      {
+        this.props.youtubeLink
+          ? <a href={this.props.youtubeLink} className={`social-links__link social-links__link--youtube social-links__link--${linkModifier}`}>
+            <i className="icon icon-youtube-with-circle"></i>
+          </a>
+        : null
+      }
     </div>
   }
 }
